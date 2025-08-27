837f8f6c4808090b19ca12230f9f36ae84484c90
shubhamkasar210 <shubhamkasar210@gmail.com>
2025-08-27 11:29:15 +0530

    signup route completed with password encryption added

--- src/app.js
+++ src/app.js
@@ -1,8 +1,40 @@
 const express = require("express");
 const app = express();
 const { connectDB } = require("./config/database");
+const bcrypt = require("bcrypt");
 
-app.use("/", (req, res) => {
+app.use(express.json());
+
+// adding middlewares
+
+const User = require("./models/user");
+
+app.post("/signup", async (req, res) => {
+  try {
+    const { userName, password, email } = req.body;
+    const hashPassword = await bcrypt.hash(password, 10);
+
+    const user = new User({
+      userName: userName,
+      email: email,
+      password: hashPassword,
+    });
+
+    await user.save(user);
+    res.send("User added successfully!!!");
+  } catch (err) {
+    res.status(400).send("User not added.");
+  }
+});
+
+app.post("/login", async (req, res) => {});
+
+app.post("/logout", async (req, res) => {});
+
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    res.status(500).send("Something went wrong.");
+  }
   res.send("Hello World");
 });
 
