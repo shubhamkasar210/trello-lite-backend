d9cf0884325cef083d41e404d34c4d53237fcab1
shubhamkasar210 <shubhamkasar210@gmail.com>
2025-08-27 08:12:21 +0530

    created a server using express

---
+++ src/app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.use("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(7777, () => {
+  console.log("server is successfully listening on port 7777");
+});
